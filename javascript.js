d6a307f8dfeb360efcf026513327195627d09cc7
Gold <94211202+Goldfromgoldwila@users.noreply.github.com>
2024-07-16 23:00:31 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.div-text h1 , h1', { delay: 50 } ) ;
+ScrollReveal().reveal('.div-text h2', { delay: 100 } ) ;
+ScrollReveal().reveal('.btn', { delay: 150 } ) ;
+ScrollReveal().reveal('.img1 , .img2 , .img3', { delay: 50 } ) ;
+
+
+ScrollReveal().reveal('.about', { delay: 200 } ) ;
+
